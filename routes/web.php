ebd5a377dbd01ea9e920141bfa2972ea38580520
mani <mani.cotginanalytics@gmail.com>
2023-07-04 15:35:35 +0530

    updated style file

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
