c6c37d0f3479d7bc3184de4a1bebaca206395ffc
Shivam Saini <shivam.cotginanalytics@gmail.com>
2023-07-04 02:38:29 -0700

    first push

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,98 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row border-bottom">
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="footer-box footer-col-1">
+                    <p>IT Services</p>
+                    <ul>
+                        <li>
+                            <a href="">Outsource Email Management</a>
+                        </li>
+                        <li>
+                            <a href="">Outsource Cybersecurity</a>
+                        </li>
+                        <li>
+                            <a href="">Outsource Data Storage</a>
+                        </li>
+                        <li>
+                            <a href="">Outsource Business Software</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="footer-box footer-col-1">
+                    <p>Marketing Solution</p>
+                    <ul>
+                        <li>
+                            <a href="">Lead Generation</a>
+                        </li>
+                        <li>
+                            <a href="">SEO</a>
+                        </li>
+                        <li>
+                            <a href="">Email Marketing</a>
+                        </li>
+                        <li>
+                            <a href="">Social & Brand Analytics</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="footer-box footer-col-1">
+                    <p>Helpdesk Support</p>
+                    <ul>
+                        <li>
+                            <a href="">Data Entry Management</a>
+                        </li>
+                        <li>
+                            <a href="">CRM integration</a>
+                        </li>
+                        <li>
+                            <a href="">Payroll Processing</a>
+                        </li>
+                        <li>
+                            <a href="">BPO</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="footer-box footer-col-1">
+                    <p>UI/UX</p>
+                    <ul>
+                        <li>
+                            <a href="">Landing Pages</a>
+                        </li>
+                        <li>
+                            <a href="">Application Design</a>
+                        </li>
+                        <li>
+                            <a href="">Website Design</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}" ></script>
+        <script src="{{ asset('js/slick.min.js') }}"></script>
+        <script src="{{ asset('js/jquery.ez.min.js') }}"></script>
+        <script src="{{ asset('js/all.min.js') }}" ></script>
+    </body>
+</html>
+
+
+
+
+
+
