573691a704ebfa5b72af13b8c3afedf7ec74e88e
Shivam Saini <shivam.cotginanalytics@gmail.com>
2023-07-04 21:27:32 -0700

    pull

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <main id="home">
     <section class="banner-home">
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col-lg-5">
                    <div class="heading">
                         <h1>Elevate your Business with Perfect Partner Today!</h1>
@@ -25,6 +25,29 @@
                         </div>
 
                     </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    <section class="about-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <p>Are you struggling to implement outsourcing for your business? You are at right place. Our team is ready to guide you through the outsourcing process. We work with businesses of all sizes and customize our services to meet each client's unique needs. Let us help you implement outsourcing successfully.</p>
+                </div>
+                <div class="col-lg-5">
+                    <div class="heading-homeh2">
+                        <h2>Power up Business with Outsourcing</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+
+                </div>
+                <div class="col-lg-8">
+                    
                 </div>
             </div>
         </div>
