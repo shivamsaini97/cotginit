1961deb1a380a53e1c4c105f0bbac665f4810e16
mani <mani.cotginanalytics@gmail.com>
2023-07-06 09:51:40 +0530

    seven pull

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,6 +4,10 @@
         .blog-big-img {
             position: relative;
         }
+
+        .latest-blog-text {
+            position: absolute;
+        }
     </style>
     <main id="blog">
 
@@ -14,12 +18,12 @@
                     <div class="col-md-12">
                         <div class="banner">
                             <div class="banner-text">
-                                <h1>About Us</h1>
+                                <h1>Blog</h1>
                                 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
                                     aria-label="breadcrumb">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="index.blade.php">Home</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">About</li>
+                                        <li class="breadcrumb-item active" aria-current="page">Blog</li>
                                     </ol>
                                 </nav>
                             </div>
@@ -31,7 +35,7 @@
         {{-- Banner end --}}
 
 
-        <section class="p-0">
+        <section>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -41,13 +45,13 @@
             </div>
         </section>
 
-        <section class="p-0">
+        <section>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="blog-big-img">
                             <img src="{{ asset('images/blog-img.jpg') }}" class="img-fluid w-100" alt="Blog">
-                            <img src="{{ asset('images/svg/latest-blog.svg') }}" alt="Blog">
+                            <img class="latest-blog-text" src="{{ asset('images/svg/latest-blog.svg') }}" alt="Blog">
                         </div>
                     </div>
                 </div>
